74d22520f6d0ab9ffac1598d68704b81bed2b4d9
a1exymoroz <a1exy.moroz@gmail.com>
2020-03-04 13:35:33 +0300

    init: init morse code decoder

--- src/index.js
+++ src/index.js
@@ -39,6 +39,42 @@ const MORSE_TABLE = {
 
 function decode(expr) {
     // write your solution here
+    const numbersLength = 10;
+    const morseLength = 2;
+    const dash = {
+        key: '11',
+        text: '-'
+    };
+    const dots = {
+        key: '10',
+        text: '.'
+    };
+    const regExp = new RegExp(`.{1,${numbersLength}}`, 'g');
+    const morseRegExp = new RegExp(`.{1,${morseLength}}`, 'g');
+    const splitExpr = expr.match(regExp);
+    const space = '**********';
+    return splitExpr.reduce((acu, cur) => {
+        if ( space === cur) {
+            acu += ' ';
+        } else {
+            const arrayMorse = cur.match(morseRegExp).map(
+                result => {
+                    let str = '';
+                    switch (result) {
+                        case dash.key:
+                            str += dash.text;
+                            break;
+                        case dots.key:
+                            str += dots.text;
+                            break;
+                    }
+                    return str;
+                }
+            );
+            acu += MORSE_TABLE[arrayMorse.join('')];
+        }
+        return acu;
+    }, '');
 }
 
 module.exports = {
